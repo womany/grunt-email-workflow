45d55a917b0c7cb3004fac66c20bb241bd3f3cf5
Lisbeth Wu <meow@liswu.me>
2020-01-02 18:25:36 +0800

    add compile list for weekly imhome

--- grunt/compile-handlebars.js
+++ grunt/compile-handlebars.js
@@ -8,5 +8,10 @@ module.exports = {
         template: '<%= paths.dist %>/weekly_site_genderpower.html',
         templateData: '<%= paths.src %>/data/weekly_site_genderpower.json',
         output: '<%= paths.dist %>/weekly_site_genderpower_with_data.html'
-    }
+    },
+  weekly_site_imhome: {
+    template: '<%= paths.dist %>/weekly_site_imhome.html',
+    templateData: '<%= paths.src %>/data/weekly_site_imhome.json',
+    output: '<%= paths.dist %>/weekly_site_imhome_with_data.html'
+  }
 };
