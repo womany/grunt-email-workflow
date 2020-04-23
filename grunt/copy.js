0de19d7caf83f0fbd2830e70f5afdb4de6e11703
Irvin <irvinfly@gmail.com>
2020-04-23 18:05:35 +0800

    add original erb build mode back and seperate comments to build_hbs & build_erb

--- grunt/copy.js
+++ grunt/copy.js
@@ -1,12 +1,21 @@
 // Replace compiled template images sources from ../src/html to ../dist/html
 module.exports = {
-  copy: {
+  handlebars: {
     files: [{
       expand: true,
       cwd: '<%= paths.dist %>',  // set working folder / root to copy
       src: "*.html",
       dest: "../",
       ext: '.handlebars'
+    }]
+  },
+  erb: {
+    files: [{
+      expand: true,
+      cwd: '<%= paths.dist %>',  // set working folder / root to copy
+      src: "*.html",
+      dest: "../",
+      ext: '.html.erb'
     }]
   }
 };
