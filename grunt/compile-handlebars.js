4207c906c655ed297d9f391f785a27377f7872a1
Irvin <irvinfly@gmail.com>
2020-05-20 18:37:38 +0800

    move settings of compile-handlebars.js to external linked file at womany repo

--- grunt/compile-handlebars.js
+++ grunt/compile-handlebars.js
@@ -1,17 +1,3 @@
-module.exports = {
-    weekly_author: {
-        template: '<%= paths.dist %>/weekly_author.html',
-        templateData: '<%= paths.src %>/data/weekly_author.json',
-        output: '<%= paths.dist %>/weekly_author_with_data.html'
-    },
-    weekly_site_genderpower: {
-        template: '<%= paths.dist %>/weekly_site_genderpower.html',
-        templateData: '<%= paths.src %>/data/weekly_site_genderpower.json',
-        output: '<%= paths.dist %>/weekly_site_genderpower_with_data.html'
-    },
-  weekly_site_imhome: {
-    template: '<%= paths.dist %>/weekly_site_imhome.html',
-    templateData: '<%= paths.src %>/data/weekly_site_imhome.json',
-    output: '<%= paths.dist %>/weekly_site_imhome_with_data.html'
-  }
+module.exports = function(grunt) {
+  return grunt.file.readJSON('hbs-data.json')
 };
